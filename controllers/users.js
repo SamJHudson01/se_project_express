e554a0b8c03c5e7a93c360b8a708f82c79551615
Samuel Hudson <samuelhudson@Samuels-MacBook-Pro.local>
2023-05-16 14:12:34 +0100

    first 3 tasks

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,9 @@
 const User = require("../models/user");
 const errors = require("../utils/errors");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../utils/config");
 
 exports.getUsers = function (req, res) {
     /* eslint-disable consistent-return */
@@ -24,14 +28,54 @@ exports.getUser = function (req, res) {
         });
 };
 
-exports.createUser = function (req, res) {
-    const user = new User(req.body);
-    user.save()
-        .orFail(new Error("Failed to create user"))
-        .exec((err, userId) => {
-            if (err) {
-                return errors.handleError(err, res);
-            }
-            res.send(userId);
+exports.createUser = async function (req, res) {
+    const { name, avatar, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        return res.status(400).send("User with this email already exists");
+    }
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        const user = new User({
+            name,
+            avatar,
+            email,
+            password: hashedPassword,
         });
+
+        user.save()
+            .then((user) => {
+                res.send(user._id);
+            })
+            .catch((err) => {
+                if (err.code === 11000) {
+                    return res.status(400).send("Duplicate value");
+                } else {
+                    return res.status(500).send("User creation failed");
+                }
+            });
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+};
+
+exports.login = async function (req, res) {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findUserByCredentials(email, password);
+        if (!user) {
+            return res.status(401).send("Invalid login credentials");
+        }
+
+        const token = jwt.sign({ _id: user._id.toString() }, JWT_SECRET, {
+            expiresIn: "7d",
+        });
+
+        res.send({ token });
+    } catch (err) {
+        res.status(401).send("Invalid login credentials");
+    }
 };
--- models/user.js
+++ models/user.js
@@ -4,19 +4,35 @@ const validator = require("validator");
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        default: "Elise Bouer", // default value
         minlength: 2,
         maxlength: 30,
     },
     avatar: {
         type: String,
-        required: true,
+        default:
+            "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Elise.png", // default value
         validate: {
             validator(value) {
                 return validator.isURL(value);
             },
             message: "You must enter a valid URL",
         },
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator(value) {
+                return validator.isEmail(value); // validate email
+            },
+            message: "You must enter a valid email",
+        },
+    },
+    password: {
+        type: String,
+        required: true,
     },
 });
 
