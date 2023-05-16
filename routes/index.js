c0b99b39f5185656869b80dbdcae50dc88fc41bd
Samuel Hudson <samuelhudson@Samuels-MacBook-Pro.local>
2023-05-16 14:28:43 +0100

    Add routes and controllers for signing up and signing in

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const userController = require("../controllers/users");
 
 const router = express.Router();
 const errors = require("../utils/errors");
@@ -13,6 +14,9 @@ router.post("/users", userController.createUser);
 router.get("/items", clothingItemsController.getClothingItems);
 router.post("/items", clothingItemsController.createClothingItem);
 router.delete("/items/:itemId", clothingItemsController.deleteClothingItem);
+
+app.post("/signin", userController.login);
+app.post("/signup", userController.createUser);
 
 router.use((req, res) => {
     res.status(errors.NOT_FOUND).json({
--- routes/user.js
+++ routes/user.js
@@ -4,32 +4,4 @@ const router = express.Router();
 const User = require("../models/user");
 const errors = require("../utils/errors");
 
-// Get all users
-router.get("/", (req, res) => {
-    User.find({})
-        .then((users) => res.json(users))
-        .catch((err) => errors.handleError(err, res));
-});
-
-// Get a specific user by id
-router.get("/:id", (req, res) => {
-    /* eslint-disable consistent-return */
-    User.findById(req.params.id)
-        .then((user) => {
-            if (!user)
-                return res
-                    .status(errors.NOT_FOUND)
-                    .json({ message: "User not found" });
-            res.json(user);
-        })
-        .catch((err) => errors.handleError(err, res));
-});
-
-// Create a new user
-router.post("/", (req, res) => {
-    User.create(req.body)
-        .then((user) => res.status(201).json(user))
-        .catch((err) => errors.handleError(err, res));
-});
-
 module.exports = router;
